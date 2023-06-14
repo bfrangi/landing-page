6baab4ceed7a00c63963917af78c6be14c8c2bf8
bfrangi <bernat.frangi@gmail.com>
2023-06-14 01:10:40 +0100

    Basic responsive layout

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+Bike1: https://www.google.com/url?sa=i&url=https%3A%2F%2Fridelines.co.uk%2Fproduct%2Fprivate-mountain-bike-tuition-half-day%2F&psig=AOvVaw1giugA13XyQm5rSmJRmxOX&ust=1686379940286000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMD7tfzMtf8CFQAAAAAdAAAAABAG  https://images.immediate.co.uk/production/volatile/sites/21/2022/09/Cervelo-Soloist-road-bike-review-19-e8ab504.jpg?quality=90&resize=620%2C413  
--- js/app.js
+++ js/app.js
@@ -1,62 +1,77 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+const sections = document.querySelectorAll("section");
+const navList = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
-// build the nav
-
+// Build the navbar by populating it with <li>. We use a document 
+// fragment to avoid reflowing the page for each <li> added to the <ul>
+document.addEventListener("DOMContentLoaded", function () {
+  const navItems = document.createDocumentFragment();
+  for (let i = 0; i < sections.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `<a href="#${sections[i].id}" class="menu__link">${sections[i].dataset.nav}</a>`;
+    navItems.appendChild(listItem);
+  }
+  navList.appendChild(navItems);
+});
 
 // Add class 'active' to section when near top of viewport
+window.addEventListener("scroll", function () {
+  for (let i = 0; i < sections.length; i++) {
+    let section = sections[i];
+    let position = section.getBoundingClientRect();
+    if (position.top <= 150 && position.bottom >= 150) {
+      section.classList.add("active-section");
+    } else {
+      section.classList.remove("active-section");
+    }
+  }
+});
 
-
-// Scroll to anchor ID using scrollTO event
-
+// Smooth scroll to anchor ID
+navList.addEventListener("click", function (event) {
+    event.preventDefault();
+    const clicked = event.target;
+    const section = document.querySelector(clicked.hash);
+    section.scrollIntoView({ behavior: "smooth" });
+});
 
 /**
  * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+ *
+ */
